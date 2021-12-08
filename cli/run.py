67a833f9104078ed1c40854d1012a374be3c16f6
George <7jagjag@gmail.com>
2021-12-08 23:29:14 +0100

    s

--- cli/run.py
+++ cli/run.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 import argparse
+import json
 import os
 import re
+from typing import Tuple
 import requests as r
 import signal
 import subprocess
 import sys
 import yaml
+import inquirer
 from argparse import ArgumentParser, Namespace
 from dataclasses import dataclass
 from emoji import emojize
+from datetime import datetime
 from termcolor import colored
 
 
@@ -27,6 +31,10 @@ class Env:
     return self.environment.get('jira').get('session')
 
   @property
+  def jira_project_key(self):
+    return self.environment.get('jira').get('project_key')
+
+  @property
   def github_host(self):
     return self.environment.get('github').get('host')
 
@@ -38,31 +46,41 @@ class Env:
   def github_repo(self):
     return self.environment.get('github').get('repo')
 
+  def set_session(self, value):
+    self.environment['jira']['session'] = value
+
   def __str__(self):
-    return yaml.dump(self.environment, Dumper=yaml.Dumper)
+    return yaml.dump(self.environment, Dumper=yaml.Dumper, sort_keys=False)
 
+home = os.environ['HOME']
+GLOBAL_CONFIG_PATH = f'{home}/.jarc.yml'
 
-def get_env(args: Namespace):
-  env = {"jira": {"host": "api.atlassian.com"}, "github": {"host": "github.com", "main_branch": "main"}}
-  home = os.environ['HOME']
-  config_path = f'{home}/.jarc.yml'
-  if os.path.isfile(config_path):
-    with open(config_path) as fh:
-      env = yaml.load(fh, yaml.Loader)
+def get_env(args: Namespace) -> Env:
+  env = {
+    "version": "1",
+    "jira": {"host": "api.atlassian.com", "session": "", "project_key": ""},
+    "github": {"host": "github.com", "main_branch": "main", "repo": ""}
+  }
+
+  if os.path.isfile(GLOBAL_CONFIG_PATH):
+    with open(GLOBAL_CONFIG_PATH) as fh:
+      result = yaml.load(fh, yaml.Loader)
+      if result:
+        env = result
   else:
-    print(colored("No config file found at ~/.jarc.yml", "yellow"))
+    print(colored(f"No config file found at {GLOBAL_CONFIG_PATH}", "yellow"))
   return Env(env)
 
 
 def main():
-  parser = argparse.ArgumentParser(description="Dev cli helper")
+  parser = argparse.ArgumentParser(prog="Dev Utils", description="Dev cli helper")
   parser.add_argument(
     "--verbose", "-v",
     help="Show more information",
     action="store_true"
   )
   parser.add_argument(
-    "--create-pr", "-c",
+    "--pr", "-p",
     help="Generate link to create a PR to be merged on the latest release branch",
     action="store_true"
   )
@@ -73,7 +91,32 @@ def main():
   )
   parser.add_argument(
     "--open", "-o",
-    help="Open either github or jira",
+    help="Open jira ticket or pr in github",
+    choices=['jira','pr']
+  )
+  parser.add_argument(
+    "--branch", "-b",
+    help="List the available branches and checkout",
+    nargs="?",
+    const='*',
+  )
+  parser.add_argument(
+    "--jira_ticket", "-j",
+    help="specify the jira ticket to avoid reading the branch name"
+  )
+  parser.add_argument(
+    "--save-session", "-s",
+    help="Save jira session cookie"
+  )
+  parser.add_argument(
+    "--update", "-u",
+    help="Update dev-utils repo",
+    action="store_true"
+  )
+  parser.add_argument(
+    "--version",
+    action="version",
+    version='%(prog)s 0.2.0'
   )
   # parser.add_argument(
   #   "--rebase", "-r",
@@ -122,9 +165,23 @@ def shell(cmd: str):
   return (error, output)
 
 
-def get_ticket_from_branch(args: Namespace):
+def get_ticket_from_branch(args: Namespace, env: Env) -> Tuple[str, str]:
+  if args.jira_ticket:
+    ticket_regex = r'([a-zA-Z]+)?-?([0-9]+)'
+    ticket_pattern = re.compile(ticket_regex)
+    result = ticket_pattern.search(args.jira_ticket)
+    if result.groups:
+      number = result.group(2)
+      if number:
+        sprint = None
+        ticket = f'{env.jira_project_key}-{number}'
+        if args.verbose:
+          print(f"sprint-number: [{colored(sprint, 'green')}]")
+          print(f"ticket-id: [{colored(ticket, 'green')}]")
+        return (sprint, ticket)
+
   branch = get_branch(args)
-  valid_branch_regex = r'^(s[0-9]+\/)?([A-Z]+-[0-9]+)(-\w+)?'
+  valid_branch_regex = r'(s[0-9]+\/)?([A-Z]+-[0-9]+)(-\w+)?'
   branch_pattern = re.compile(valid_branch_regex)
   result = branch_pattern.search(branch)
   if not result:
@@ -174,19 +231,23 @@ class Cli:
     if self.args.verbose:
       print(f"{self.args}\n\n{self.env}")
 
-    if self.args.create_pr:
-      self.create_pr()
+    if self.args.pr:
+      self.pr()
     elif self.args.desc:
       self.desc()
     elif self.args.open:
       self.open()
+    elif self.args.branch:
+      self.branch()
+    elif self.args.save_session:
+      self.save_session()
     elif self.args.verbose:
       pass
     else:
       parser.print_help()
 
   def open(self):
-    (branch, ticket) = get_ticket_from_branch(self.args)
+    (branch, ticket) = get_ticket_from_branch(self.args, self.env)
     url = None
     if self.args.open == 'j':
       url = (
@@ -211,32 +272,79 @@ class Cli:
         print(url)
       os.system(f'xdg-open "{url}"')
 
+  def save_session(self):
+    self.env.set_session(self.args.save_session)
+    with open(GLOBAL_CONFIG_PATH, 'w') as fh:
+      fh.write(f"{self.env}")
+      print(f"update rc [{colored('done', 'green')}]")
+
   def desc(self):
-    (branch, ticket) = get_ticket_from_branch(self.args)
-    r = self.jira.get(f'/rest/api/2/issue/{ticket}?fields=summary,description')
+    (branch, ticket) = get_ticket_from_branch(self.args, self.env)
+    r = self.jira.get(f'/rest/api/2/issue/{ticket}?') #fields=summary,description
     if r is None:
       exit(1)
     summary = r['fields']['summary']
     description = r['fields']['description']
-    print(f"\n{emojize(':memo:')} {summary}")
+    points = r['fields']['customfield_10006']
+    points = f'({points}) ' if points else ''
+    github = r['fields']['customfield_11100']
+    pr_status = github.split(', details=PullRequestOverallDetails')[0].split('state=')[1]
+    pr_status = colored(f'{pr_status} ', 'yellow')
+    comments = r['fields']['comment']
+    owner = None
+
+    # for k in r['fields'].keys():
+    #   print(f"{k}|| {r['fields'][k]}")
+    print(f"\n{emojize(':memo:')} {pr_status}{points}{summary}")
+    if owner:
+      print(f"owner: {owner}")
     print("")
     print(emojize(description))
+    for c in comments['comments']:
+      display_name = c['author']['displayName']
+      body = c['body']
+      updated = datetime.strptime(c['updated'], '%Y-%m-%dT%H:%M:%S.%f%z').strftime('%Y-%m-%d %H:%M %z')
+      print(f'{colored(updated, "blue")} {display_name}: {body}')
 
-  def create_pr(self):
-    (branch, ticket) = get_ticket_from_branch(self.args)
+  def branch(self):
+    (err, output) = shell('git branch')
+    if err:
+      print(colored(err, 'red'))
+      exit(1)
+
+    branches = [''.join(branch.split('*')).strip() for branch in output.split('\n')]
+    if self.args.branch != '*':
+      branches = [branch for branch in branches if self.args.branch in branch]
+    questions = [
+      inquirer.List('branch', message="What branch?", choices=branches)
+    ]
+    answers = inquirer.prompt(questions)
+    branch = answers.get('branch')
+    (err, output) = shell('git status --porcelain --untracked-files=no')
+    if output=="":
+      shell(f"git checkout {branch}")
+    elif err:
+       print(colored(err, 'red'))
+    else:
+      print(colored('Expected clean directory, please commit or stash your pending changes', 'yellow'))
+      print(output)
+
+  def pr(self):
+    (branch, ticket) = get_ticket_from_branch(self.args, self.env)
     link = (
       f"https://{self.env.github_host}/"
       f"{self.env.github_repo}/compare/{self.env.github_main_branch}...dev:"
       f"{branch}"
     )
 
-    response = self.jira.get(f'/rest/api/2/issue/{ticket}?fields=summary')
+    response = self.jira.get(f'/rest/api/2/issue/{ticket}?fields=summary,customfield_10006')
 
     print("\nPull Request")
 
     if response:
       summary = response['fields']['summary']
-      name = f"[{ticket}] - {summary}"
+      points = response['fields']['customfield_10006']
+      name = f"[{ticket}] - ({points}) {summary}"
       copy_to_clipboard(name)
       print(f"name: {colored(name, 'yellow')} # copied to your clipboard!")
 
